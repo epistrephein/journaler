61042713de9d70915014e594e369d783ef7e3e0a
nicoldamelio <nicoldamelio2@gmail.com>
2025-05-19 11:13:02 +0200

    journaluploadhandler
    classe JUH

--- impl/handlers.py
+++ impl/handlers.py
@@ -29,11 +29,58 @@ class JournalUploadHandler(UploadHandler):
         super().__init__()
 
     def pushDataToDb(self, path):
-        if not self.getDbPathOrUrl():
-            return False
 
-        print(f"Pushing {path} to {self.getDbPathOrUrl()}")
+        if not self.getDbPathOrUrl():
+            raise ValueError("Database path or URL is not set")
+
+        if not path.ends_with(".csv"):
+            raise ValueError("In sert csv file")
+
+
+        
+        # class
+        Journal = URIRef ("https://schema.org/Periodical")
+
+        # attributes of the class
+        title = URIRef("https://schema.org/name")
+        issn = URIRef("https://schema.org/identifier")
+        eissn = URIRef("https://schema.org/identifier")
+        language = URIRef ("https://schema.org/inLanguage")
+        publisher = URIRef ("https://schema.org/publisher")
+        seal = URIRef ("https://www.wikidata.org/wiki/Q73548471")
+        license = URIRef ("https://schema.org/license")
+        apc = URIRef ("https://www.wikidata.org/wiki/Q15291071")
+
+        base_url= "https://github.com/epistrephein/journaler/"
+
+        df = read_csv (path,
+                       keep_default_na=False)
+        
+        graph = Graph()
+        df = df[100]
+
+        for idx, row in df.iterrows():
+            local_id = "journal-" + str(idx)
+            subj = URIRef(base_url + local_id)
+            graph.add ((subj, RDF.type, Journal))
+            graph.add ((subj, title, Literal(row["Journal title"])))
+            graph.add ((subj, issn, Literal (row["Journal ISSN (print version)"])))
+            graph.add ((subj, eissn, Literal (row["Journal EISSN (online version)"])))
+            graph.add ((subj, language, Literal (row["Languages in which the journal accepts manuscripts"])))
+            graph.add ((subj, publisher, Literal(row["Publisher"])))
+            graph.add ((subj, seal, Literal(row["Seal"])))
+            graph.add ((subj, license, Literal (row["Journal license"])))
+            graph.add ((subj, apc, Literal(row["APC"])))
+
+        store = SPARQLUpdateStore()
+        endpoint = getDbPathOrUrl()
+        store.open((endpoint, endpoint))
+        for triple in graph.triples (None, None, None):
+            store.add(triple)
+        store.close()
+
         return True
+        
 
 class CategoryUploadHandler(UploadHandler):
     def __init__(self):
